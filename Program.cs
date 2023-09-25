b3cae5d2006ff7630cb16f67b59eca94de2872e4
Ahmedm100 <144054939+Ahmedm100@users.noreply.github.com>
2023-09-25 13:14:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    // den här kommandon WriteLine skriver ut , hello world i powershell
+             // du kan skriva vad du vill efter Writeline
+             // console matar in det du vill skriva till writeline som en command 
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
